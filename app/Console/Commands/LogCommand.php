7d0a032335ceb85ed42ea545001ad1113b62acd1
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-05-26 15:23:03 +0800

    command log

---
+++ app/Console/Commands/LogCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class LogCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'createlog';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'To create laravel log';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return Log::info('This is laravel log by command');
+    }
+}
