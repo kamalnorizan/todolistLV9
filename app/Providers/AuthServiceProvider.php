1f7bc7c1c5c34bd2f2fe7467eb76905144757ad8
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-05-27 11:10:31 +0800

    Token Expired

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
+use Carbon\Carbon;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -25,10 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        if(! $this->app->routesAreCached()){
-            Passport::routes();
-        }
+        Passport::routes();
 
-        //
+        Passport::tokensExpireIn(Carbon::now()->addDays(1));
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addDays(30));
+
     }
 }
